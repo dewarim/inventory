02b7efa7f2ec3cd3479a06aedb07b3121164fa7e
Ingo Wiarda <ingo_wiarda@dewarim.de>
2015-01-26 20:35:55 +0100

    Add hashCode/equals/toString

--- grails-app/domain/inventory/Book.groovy
+++ grails-app/domain/inventory/Book.groovy
@@ -14,5 +14,38 @@ class Book {
     String authorName
     String comment
     String tags
-    
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", authorName='" + authorName + '\'' +
+                ", comment='" + comment + '\'' +
+                ", tags='" + tags + '\'' +
+                '}';
+    }
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (!(o instanceof Book)) return false
+
+        Book book = (Book) o
+
+        if (authorName != book.authorName) return false
+        if (comment != book.comment) return false
+        if (id != book.id) return false
+        if (tags != book.tags) return false
+        if (title != book.title) return false
+
+        return true
+    }
+
+    int hashCode() {
+        int result
+        result = title.hashCode()
+        result = 31 * result + authorName.hashCode()
+        return result
+    }
 }
